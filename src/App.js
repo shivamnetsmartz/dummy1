ca6347fbdb7a3944d06d2e3460e5e10bcaa03a56
bhatiashivam5 <bhatiashivam35@gmail.com>
2021-10-19 18:41:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {useState, useMemo} from 'react';
 import './App.css';
 
 function App() {
+
+
+  const [counter, setCounter] = useState(1);
+  const result = useMemo(() => {
+    return factorial(counter);
+  }, [counter]);
+ const [name, setName] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Factorial of {counter} is: <span>{result}</span></h1>
+
+      <div>
+        <button onClick={() => setCounter(counter + 1)}>Increment</button>
+        <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      </div>
+      <hr></hr>
+
+      <div>
+        <div>
+          <label>Enter Name</label>
+        </div>
+        <input 
+        type="text"
+        placeholder="enter name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        />
+<hr></hr>
+<DisplayName name={name}></DisplayName>
+      </div>
     </div>
   );
+}
+
+const DisplayName = useMemo(({name}) =>{
+  console.log("running");
+  return <p>{`My Name is ${name}`}</p>;
+});
+
+
+function factorial(n) {
+  let i=0;
+  while (i<2000000000)
+  i++;
+  if(n<0){
+    return -1;
+  }
+  if(n === 0){
+    return 1;
+  }
+  return n*factorial(n-1);
 }
 
 export default App;
